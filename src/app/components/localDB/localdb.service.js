57f6444f384c820987bc50038f9e3854f0d52f99
Ricco <ricco@snsoft.my>
2017-02-13 14:39:18 +0800

    code enhancement on localdb factory service

--- src/app/components/localDB/localdb.service.js
+++ src/app/components/localDB/localdb.service.js
@@ -7,155 +7,100 @@
 
   /** @ngInject */
   function localdb($log, $window, $q) {
-    var dbName = "snsofthrdb";
+    var DB_NAME = "snsofthrdb";
+    var DB_VERSION = 2;
+    var db;
+
+    openDb();
 
     var service = {
-      getLocal : getLocal,
       addUser : addUser,
       getUser: getUser
     };
 
     return service;
 
-    function getLocal() {
-      // In the following line, you should include the prefixes of implementations you want to test.
-      //$window.indexedDB = $window.indexedDB || $window.mozIndexedDB || $window.webkitIndexedDB || $window.msIndexedDB;
-      // DON'T use "var indexedDB = ..." if you're not in a function.
-      // Moreover, you may need references to some $window.IDB* objects:
-      //$window.IDBTransaction = $window.IDBTransaction || $window.webkitIDBTransaction || $window.msIDBTransaction || {READ_WRITE: "readwrite"}; // This line should only be needed if it is needed to support the object's constants for older browsers
-      //$window.IDBKeyRange = $window.IDBKeyRange || $window.webkitIDBKeyRange || $window.msIDBKeyRange;
-      // (Mozilla has never prefixed these objects, so we don't need $window.mozIDB*)
-
-      if (!$window.indexedDB) {
-        $window.alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
-      }
-
-      // Open database
-      var dbName = "snsofthrdb";
-
-      var db;
-      var request = indexedDB.open(dbName, 2);
-
-      // Open database - Response
-      request.onerror = function(event) {
-        // Open database - Error
-        alert("Database error: " + event.target.errorCode);
-      };
-      request.onupgradeneeded = function(event) {
-        var db = event.target.result;
-
-        // Create an objectStore to hold information about our customers. We're
-        // going to use "ssn" as our key path because it's guaranteed to be
-        // unique - or at least that's what I was told during the kickoff meeting.
-        var objectStore = 
-          db.createObjectStore("user", { keyPath : "username", autoIncrement : true });
-      };
-      request.onsuccess = function(event) {
-        // Open database - Success
-        $log.info("Success open database");
-        db = event.target.result;
-      };
-
-
-
-    }
-
     function addUser(username, password) {
-      var db;
-      var request = indexedDB.open(dbName, 2);
+      //var deferred = $q.defer();
+      var DB_STORENAME = 'user';
 
       var newUser = {
         username: username,
         password: password
       }
 
-      $log.info(newUser);
+      var request = 
+        getObjectStore(DB_STORENAME, 'readwrite')
+        .add(newUser);
 
-      // Open database - Response
       request.onerror = function(event) {
-        // Open database - Error
-        alert("Database error: " + event.target.errorCode);
-      };
-      request.onupgradeneeded = function(event) {
-        var db = event.target.result;
+        // Add user trasaction - Error
+        alert("Transaction error: " + event.target.errorCode);
+      }; 
 
-        // Create an objectStore to hold information about our customers. We're
-        // going to use "ssn" as our key path because it's guaranteed to be
-        // unique - or at least that's what I was told during the kickoff meeting.
-        var objectStore = 
-          db.createObjectStore("user", { keyPath : "username", autoIncrement : true });
-      };
+      // Do something when all the data is added to the database.
       request.onsuccess = function(event) {
-        // Open database - Success
-        $log.info("Success open database");
-        db = event.target.result;
-
-        var transaction = db.transaction(["user"], "readwrite");
-        var userObjStore = transaction.objectStore("user");
-        var request = userObjStore.add(newUser);
-
-        // Do something when all the data is added to the database.
-        transaction.oncomplete = function(event) {
-          $log.info("Execute add");
-        };
-
-        transaction.onerror = function(event) {
-          // Add user trasaction - Error
-          alert("Transaction error: " + event.target.errorCode);
-        };        
+        $log.info("Execute add");
       };
     }
 
     function getUser(username) {
       var deferred = $q.defer();
+      var DB_STORENAME = 'user';
 
-      var db;
-      var request = indexedDB.open(dbName, 2);
+      var request = 
+        getObjectStore(DB_STORENAME, 'readonly')
+        .get(username);
 
-      // Open database - Response
       request.onerror = function(event) {
-        // Open database - Error
-        alert("Database error: " + event.target.errorCode);
-      };
-      request.onupgradeneeded = function(event) {
-        var db = event.target.result;
-
-        // Create an objectStore to hold information about our customers. We're
-        // going to use "ssn" as our key path because it's guaranteed to be
-        // unique - or at least that's what I was told during the kickoff meeting.
-        var objectStore = 
-          db.createObjectStore("user", { keyPath : "username", autoIncrement : true });
-      };
-      request.onsuccess = function(event) {
-        // Open database - Success
-        $log.info("Success open database");
-        db = event.target.result;
-
-        var transaction = db.transaction(["user"]);
-        var userObjStore = transaction.objectStore("user");
-        var request = userObjStore.get(username);
-
-        request.onerror = function(event) {
-          $log.info("User not exist!");
-          return false;
-        };    
-        // Do something when all the data is added to the database.
-        request.onsuccess = function(event) {
-          var value = event.target.result;
-
-          if (value) {
-            $log.info("User found!");
-            deferred.resolve(value);
-          } else {
-            $log.info("User not exist!");
-            deferred.reject("User not exist!");
-          }
-        };
-
-        
+        $log.info("Open ObjectStore Error!");
       };    
+      // Do something when all the data is added to the database.
+      request.onsuccess = function(event) {
+        var value = event.target.result;
+
+        if (value) {
+          $log.info("User found!");
+          deferred.resolve(value);
+        } else {
+          $log.info("User not exist!");
+          deferred.reject("User not exist!");
+        }
+      };
 
       return deferred.promise;  
+    }
+
+    function openDb() {
+      $log.info("openDb ...");
+      var req = indexedDB.open(DB_NAME, DB_VERSION);
+      req.onsuccess = function (evt) {
+        // Better use "this" than "req" to get the result to avoid problems with
+        // garbage collection.
+        // db = req.result;
+        db = this.result;
+        $log.info("openDb DONE");
+      };
+      req.onerror = function (evt) {
+        $log.info("openDb:", evt.target.errorCode);
+      };
+
+      req.onupgradeneeded = function (evt) {
+        $log.info("openDb.onupgradeneeded");
+        var store = 
+          evt.currentTarget.result
+          .createObjectStore("user", { keyPath : "username", autoIncrement : true });
+      };
+    }
+
+    /**
+     * @param {string} store_name
+     * @param {string} mode either "readonly" or "readwrite"
+     */
+    function getObjectStore(store_name, mode) {
+      $log.info("getObjectStore: " + db);
+      var tx = db.transaction(store_name, mode);
+      return tx.objectStore(store_name);
     }
   }
 })();
