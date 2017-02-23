fee04a38459fcbaa0b6b0b660283aec63acd8031
Ricco <ricco@snsoft.my>
2017-02-23 18:26:59 +0800

    apply leave dialog skeleton

--- src/app/leaveMgmt/leaveMgmt.controller.js
+++ src/app/leaveMgmt/leaveMgmt.controller.js
@@ -6,16 +6,54 @@
 		.controller('LeaveMgmtController', LeaveMgmtController);
 
 	/** @ngInject */
-	function LeaveMgmtController() {
+	function LeaveMgmtController($mdDialog, $document) {
 		var vm = this;
-		
+
 		// Function Declaration
-    
+        vm.newLeave = newLeave;
+
 		// Load Departments Table
 
 		//// Public Functions
+        function newLeave (ev) {
+            $mdDialog.show({
+                controller: DialogController,
+                controllerAs: 'dialog',
+                templateUrl: '/app/leaveMgmt/leaveMgmt.newLeave.html',
+                parent: angular.element($document.body),
+                targetEvent: ev,
+                clickOutsideToClose:true,
+                fullscreen: vm.customFullscreen // Only for -xs, -sm breakpoints.
+            })
+            .then(function(answer) {
+                vm.status = 'You said the information was "' + answer + '".';
+            }, function() {
+                vm.status = 'You cancelled the dialog.';
+            });
+        }
 
 		//// Private Functions
+        function DialogController($log, $mdDialog, systemServ) {
+            var vm = this;
+
+            vm.leaveTypes = {
+                1: "Annual Leave",
+                2: "Medical Leave",
+                99: "Other Reason"
+            }
+
+            vm.hide = function() {
+                $mdDialog.hide();
+            };
+
+            vm.cancel = function() {
+                $mdDialog.cancel();
+            };
+
+            vm.answer = function(answer) {
+                $mdDialog.hide(answer);
+            };
+        }
 
 	}
 })();
