1e6d2e91ce1d2972201aafc550fd3a10747decd5
ricco <ricco@snsoft.my>
2017-03-16 16:21:41 +0800

    bug fix on leave management view

--- src/app/leaveMgmt/leaveMgmt.controller.js
+++ src/app/leaveMgmt/leaveMgmt.controller.js
@@ -10,7 +10,7 @@
   function LeaveMgmtController($mdDialog, $document, $timeout, $cookies, $log, leaveServ, AuthService, syncData) {
     var vm = this;
     var id = 3;
-    vm.leaves;
+
     // Function Declaration
     vm.newLeave = newLeave;
     vm.approveLeave = approveLeave;
@@ -34,12 +34,7 @@
         targetEvent: ev,
         clickOutsideToClose:true,
         fullscreen: vm.customFullscreen // Only for -xs, -sm breakpoints.
-      })
-        .then(function(answer) {
-          vm.status = 'You said the information was "' + answer + '".';
-        }, function() {
-          vm.status = 'You cancelled the dialog.';
-        });
+      });
 
       function DialogController($log, $mdDialog, $cookies, leaveServ, toastr) {
         var vm = this;
@@ -100,13 +95,17 @@
       $timeout(function() {
         leaveServ.getPendingApprovalLeaveByUsername(curUser.username).then(function(leaves) {
           vm.leavesPendingMyApprove = leaves;
+
+          // get all leaves pending approve from department
           if (curUser.position === "Department Head") {
-            leaveServ.getPendingApprovalLeaveByDepartment(curUser.department).then(function(leaves) {
-              leaves.forEach(function(leave) {
-                // if (!vm.leavesPendingMyApprove.find(x => x._id === leave._id)) {
-                //   vm.leavesPendingMyApprove.push(leave);
-                // }
-              });
+            leaveServ.getPendingApprovalLeaveByDepartment(curUser.department)
+              .then(function(leaves) {
+                leaves.forEach(function(leave) {
+                  if (!vm.leavesPendingMyApprove.find(function(x) { return x.indexID === leave.indexID })
+                        && curUser.username !== leave.user) {
+                    vm.leavesPendingMyApprove.push(leave);
+                  }
+                });
             });
           }
         });
